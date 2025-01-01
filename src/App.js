388eeaa9412aea2163dca8961ce42989d7725b7a
1yavuz <1yavuzahmet@gmail.com>
2025-01-01 21:24:41 +0300

    router erase

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import { Route, Routes, BrowserRouter } from "react-router-dom"
 import './App.css';
 import Header from './components/Header';
 import CardContainer from './components/CardContainer';
-import Other from "./components/Other";
 
 
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="text-center mt-4">
-        <Header />
-        <Routes>
-          <Route path="/" element={<CardContainer />} />
-          <Route path="/other" element={<Other />} />
-        </Routes>
 
-      </div>
-    </BrowserRouter>
+    <div className="text-center mt-4">
+      <Header />
+      <CardContainer />
+
+    </div>
+
   );
 
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Image } from "react-bootstrap"
 import logo from "../assets/nba-logo.png"
-import { NavLink } from 'react-router-dom'
+
 
 
 const Header = () => {
@@ -9,10 +9,7 @@ const Header = () => {
         <div>
             <Image src={logo}></Image>
             <h1 className='fw-bold my-1 display-3'>NBA Legends</h1>
-            <nav className="d-flex gap-4 fs-5">
-                <NavLink to={"/"}>Anasayfa</NavLink>
-                <NavLink to={"/other"}>Other Pages</NavLink>
-            </nav>
+
         </div>
     )
 }
